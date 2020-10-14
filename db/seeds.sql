7fb032e775fc3abefdead964720687a477fc8311
Quint Turner <henryquintturner@gmail.com>
2020-10-14 15:54:33 -0700

    Created DB folder with seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Quarter Pounder");
+INSERT INTO BURGERS (burger_name) VALUES ("Impossible Burger");
